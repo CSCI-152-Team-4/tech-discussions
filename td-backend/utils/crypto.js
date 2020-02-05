30805eb36b858aabdd223fdd7a580f00663740f1
Andrew <andydrew313@gmail.com>
2020-02-04 22:20:34 -0800

    remove backend

--- td-backend/utils/crypto.js
+++
@@ -1,9 +0,0 @@
-var crypto = require('crypto');
-
-var encryptPass = (pass) => {
-  var sha = crypto.createHash('sha256');
-  sha.update(pass);
-  return sha.digest('hex');
-}
-
-module.exports.encryptPass = encryptPass;
