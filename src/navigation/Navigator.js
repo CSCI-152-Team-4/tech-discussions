738b435b3700326eafa4954b12856970eafc13bd
Andrew <andydrew313@gmail.com>
2020-02-05 19:28:15 -0800

    adding navigation skeleton

---
+++ src/navigation/Navigator.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+
+import Routes from './Routes'
+
+const Navigator = () => {
+  return(
+    <Switch>
+      { 
+        Routes.map(({Component, name, path, locked}, index)=>{
+          return (
+            locked ?
+              <PrivateRoute exact path={path} Component={Component} key={`p-index-${index}`} loggedIn={true}/> // todo add auth system
+              :
+              <Route exact path={path} key={`p-index-${index}`} render={(props) => (
+                <Component {...props} />
+              )} />
+          )
+        })
+      }
+      <Route exact path="/" render={(props) => (
+        <Redirect {...props} to= '/home'/>
+      )}/>
+      <Route render={(props) => ( // todo add 404 screen and more redirects maybe
+        <div>not found 404</div>
+      )} /> 
+    </Switch>
+  )
+}
+
+const PrivateRoute = ({Component, loggedIn, ...rest}) => (
+  <Route {...rest} render={(props) => (
+    loggedIn ?
+      (<>
+        <div style={{height: '10vh'}}> {/* todo add header nav component*/}
+          header
+        </div>
+        <div style={{height: '90vh'}}>
+          <Component {...props} />
+        </div>
+      </>)
+      :
+      (<Redirect {...props} to='/login'/>)
+  )} />
+)
+
+export default Navigator
---
+++ src/navigation/Routes.js
@@ -0,0 +1,18 @@
+import Components from '../screens'
+
+const Routes = [
+  {
+    Component: Components.Home,
+    name: 'Home',
+    path: '/home',
+    locked: true
+  },
+  {
+    Component: Components.Login,
+    name: 'Login',
+    path: '/login',
+    locked: false
+  }
+]
+
+export default Routes
