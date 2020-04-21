32d4141fe56aae5bf5fb88b2f344d11893155bd0
Josh <joshualaofrancisco@hotmail.com>
2020-04-20 23:02:42 -0700

    update sign up
    sign up page similiar to log in page now

--- src/screens/login/LoginScreen.js
+++ src/screens/login/LoginScreen.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Checkbox from "@material-ui/core/Checkbox";
@@ -11,6 +12,7 @@ import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import { useHistory } from "react-router-dom";
 import { useStoreState, useStoreActions } from "easy-peasy";
 
@@ -67,6 +69,7 @@ export default function SignIn() {
   const { loggedIn, loginError } = useStoreState(({ User }) => User);
   const [creds, setCreds] = useState({ email: "", password: "" });
   const [err, setErr] = useState("");
+  
   useEffect(() => {
     setLoggedIn(false);
   }, [setLoggedIn]); // set logged out
@@ -89,7 +92,7 @@ export default function SignIn() {
           )
         )
           login(creds);
-        else setErr("Email must be a Fresno State account only");
+        else setErr("Email must be a Fresno State Account only");
       }
     },
     [creds.email, creds.password, creds, login]
@@ -117,9 +120,7 @@ export default function SignIn() {
             Sign In
           </Typography>
           <form className={classes.form} noValidate>
-            <Typography style={{ textAlign: "center", color: "red" }}>
-              {err}
-            </Typography>
+            <Typography style={{ textAlign: "center", color: "red" }}> {err} </Typography>
             <TextField
               variant="outlined"
               margin="normal"
