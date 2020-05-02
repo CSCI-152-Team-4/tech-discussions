795e7dd821c62b517812c00e8da0fcc4aa0cdc3d
avromero <49049015+avromero@users.noreply.github.com>
2020-05-02 16:01:19 -0700

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+const assert = require('assert'),
+  puppeteer = require('puppeteer'); // will have to add puppeteer to yarn
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+/*   This will open a virtual bowser and run test on that     */
+let browser, page;
+beforeEach(async () =>{
+  browser = await puppeteer.launch(//{
+    //headless : true
+  //}
+  );
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000/login");
+
+});
+afterEach(async()=> {
+  await browser.close();
+});
+
+test('clicking button', async ()=>{
+//await page.click("Button.MuiButtonBase-root");
+const url = await page.url();
+console.log(url);
+expect(url).toMatch("http://localhost:3000/login");
 });
