34f7d414ca137f77e1153e9b8484b20a72841d20
Andrew <andydrew313@gmail.com>
2020-05-04 21:24:40 -0700

    add some tests

---
+++ src/services/services.test.js
@@ -0,0 +1,26 @@
+import AuthService from './Authentication'
+import PostService from './Posts'
+
+//auth
+it('logs in users', async () => {
+  const res = await AuthService.login("andrewadams3@mail.fresnostate.edu", "test")
+  expect(res.status).toBe('success')
+})
+
+it('catches incorrect password', async () => {
+  const res = await AuthService.login("andrewadams3@mail.fresnostate.edu", "123")
+  expect(res.status).toBe('Incorrect Credentials')
+})
+
+it('catches account not found on login', async () => {
+  const res = await AuthService.login("andrewadams3432@mail.fresnostate.edu", "123")
+  expect(res.status).toBe('User Not Found')
+})
+
+//posts
+it('gets posts', async () => {
+  const res = await PostService.getPosts(1)
+  expect(res instanceof Array).toBe(true)
+  expect(res.length === 1).toBe(true)
+  expect(typeof res[0].title === "string").toBe(true)
+})
